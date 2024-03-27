2b31c6a67da2ff688037e899da973265f69dbf21
NelvaAdalit <moraadalid378@gmail.com>
2024-03-26 20:41:16 -0400

    Agregar archivos de proyecto.

---
+++ Intermediate/Build/Win64/Galaga_USFXEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.39.33522
+#include "C:/Users/CAPITAL COMPUTERS/Downloads/Galaga_USFX-master/Galaga_USFX-master/Galaga_USFX/Intermediate/Build/Win64/Galaga_USFXEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
